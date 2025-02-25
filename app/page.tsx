7031c6ba420adcba5c79fa16b1a7b9cb2c5ee1e3
Sayan-009 <sayankhanra019@gmail.com>
2025-02-25 17:17:13 +0530

    updated file

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,14 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 import RollingGallery from "@/components/reactBits/RollingGallary";
 import StickyScrollComponenet from "@/components/StickyScrollComponenet";
 import BentoGridComponent from "@/components/BentoGridComponent";
+import SplashCursor from "@/components/ui/splash-cursor";
 
 export default function Home() {
   return (
+
+    
     <div className="min-h-screen bg-stone-950 flex flex-col items-center w-full">
+      <SplashCursor/>
       <div className="h-screen w-full m-0 p-0 border border-red-800">
         <Image src={img} className="cover" alt="no image found"></Image>
       </div>
